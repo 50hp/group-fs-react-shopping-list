bf61a058aa39ea8c84197d0b0c1c1d651f97799f
Conrad Rolf <williamconradrolf@gmail.com>
2023-06-01 11:15:22 -0500

    added GET function to the App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
-
+import {useState, useEffect} from 'react'
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+
+    const [shoppingList, setShoppingList] = useState([])
+
+    const getShoppingList = () => {
+        axios.get('/shopping').then(response => {
+            setShoppingList(response.data)
+        }).catch(error => {
+            console.log('Error with GET request', error)
+            alert('Error getting your shopping list, try again later')
+        })
+    }
+
+    useEffect(() => {
+        getShoppingList();
+    }, [])
+
     return (
         <div className="App">
             <Header />
             <main>
                 <p>Under Construction...</p>
+                <ul>
+                    {shoppingList.map(item => (
+                        <li key={item.id}>
+                            {item.name} 
+                            {item.quantity} {item.unit}
+                            <button>Buy</button><button>Delete</button>
+                        </li>
+                    ))}
+                </ul>
             </main>
         </div>
     );
